99f74e44a18a7c0f7c040db49c122f2038ac6b25
wL1u <1970650352@qq.com>
2020-12-19 20:21:09 +0800

    add app.py and 2 *.html files for Flask

---
+++ src/app.py
@@ -0,0 +1,44 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from flask import Flask, render_template, request, redirect, url_for, abort
+from PIL import Image, ImageOps
+
+import _init_paths
+import os
+import cv2
+
+from opts import opts
+from detectors.detector_factory import detector_factory
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET", "POST"])
+def upload_file():
+    os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpus_str
+    opt.debug = max(opt.debug, 1)
+    Detector = detector_factory[opt.task]
+    detector = Detector(opt)
+
+    if request.method == "GET":
+        return render_template("upload.html")
+    if request.method == "POST":
+        # アプロードされたファイルをいったん保存する
+        f = request.files["file"]
+        filepath = "./static/flask_input.jpg"
+        # f.save(filepath)
+        # # 画像ファイルを読み込む
+        file = [filepath]
+        # image = Image.open(file[0])
+
+        # 予測を実施
+        detector.run(file[0])
+
+        return render_template("upload_done.html", path="./static/flask_output.jpg")
+
+
+if __name__ == "__main__":
+    opt = opts().init()
+    app.run(debug=True)
